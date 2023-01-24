f9e17d097ff8c7e9b2dc91d202666f6f4281616f
hasankara80 <hasankara80@gmail.com>
2023-01-24 13:52:51 +0000

    Add Player and Camera Movement

--- Assets/_GameData/Scripts/CameraFollow.cs
+++ Assets/_GameData/Scripts/CameraFollow.cs
@@ -1,18 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    /*[SerializeField] private Transform target;
+    private float _smoothSpeed;
+    private Vector3 _offset;
+
+    private void Start()
     {
-        
+        _smoothSpeed = 0.125f;
+        _offset = new Vector3(0, 22, 0);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        Vector3 desiredPosition = target.position + _offset;
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, desiredPosition, _smoothSpeed);
+        transform.position = smoothPosition;
+    }*/
+    
+    private bool _isStartMovementCompleted;
+    private void Start()
+    {
+        _isStartMovementCompleted = false;
+        transform.DOMoveX(0, 2).SetEase(Ease.InCubic).OnComplete(() =>
+        {
+            _isStartMovementCompleted = true;
+        });
+    }
+
+    private void Update()
+    {
+        if (!_isStartMovementCompleted) return;
+        transform.Translate(Vector3.down * 2 * Time.deltaTime);
     }
 }
--- Assets/_GameData/Scripts/PlayerMovement.cs
+++ Assets/_GameData/Scripts/PlayerMovement.cs
@@ -1,33 +1,30 @@
-using System;
 using DG.Tweening;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private bool isStartMovementCompleted;
+    private bool _isStartMovementCompleted;
     private void Start()
     {
-        isStartMovementCompleted = false;
+        _isStartMovementCompleted = false;
         transform.DOMoveX(0, 2).SetEase(Ease.InCubic).OnComplete(() =>
         {
-            isStartMovementCompleted = true;
+            _isStartMovementCompleted = true;
         });
     }
 
     private void Update()
     {
-        if (isStartMovementCompleted)
+        if (!_isStartMovementCompleted) return;
+        transform.Translate(Vector3.up * 2 * Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            transform.Translate(Vector3.up * 2 * Time.deltaTime);
-            if (Input.GetKeyDown(KeyCode.RightArrow))
-            {
-                transform.DOMoveX(transform.position.x - 2, 0.5f);
-            }
+            transform.DOMoveX(transform.position.x - 2, 0.5f);
+        }
 
-            if (Input.GetKeyDown(KeyCode.LeftArrow))
-            {
-                transform.DOMoveX(transform.position.x + 2, 0.5f);
-            }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.DOMoveX(transform.position.x + 2, 0.5f);
         }
     }
 }
